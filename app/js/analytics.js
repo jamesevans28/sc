1167bdd65a8aee88313f9ad13d90478e5ace6311
James <james.evans.2@team.telstra.com>
2017-03-27 11:00:10 +1100

    Google analytics

---
+++ app/js/analytics.js
@@ -0,0 +1,10 @@
+angular.module('supercoach')
+.run(function ($rootScope, $state, $window, $location) {
+
+  $window.ga('create', 'UA-96202267-1', 'auto');
+
+   $rootScope.$on('$stateChangeSuccess', function (event) {
+      $window.ga('send', 'pageview', $location.path());
+   });
+
+});
